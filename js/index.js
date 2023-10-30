e27468e9ac0cf07fa509c426b21335aa785814f9
Tharun Raj <tharun.2.linux@gmail.com>
2023-10-30 21:33:35 +0530

    small edits
    - changed homepage name
    - added about.html file.
    - active links now trigger an alert when clicked.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
+// Footer year
 const footerDate = document.getElementById("currentYear");
 
 let d = new Date();
 const currentYear = d.getFullYear();
 footerDate.textContent = currentYear;
+
+// For active links
+let activeLink = document.querySelector(".active");
+
+activeLink.addEventListener("click", () => {
+  alert("You're already there!");
+});
