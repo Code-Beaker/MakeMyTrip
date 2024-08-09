045a449b79ced92742bad9ccca2cc3024bdda193
Tharun Raj <tharun.2.linux@gmail.com>
2024-08-09 18:10:55 +0530

    make toggle icons switch source

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import menuOpen from "./img/compressed/menu_26dp_FFF_FILL0_wght400_GRAD0_opsz24.svg";
+import menuClose from "./img/compressed/close_26dp_FFF_FILL0_wght400_GRAD0_opsz24.svg";
+
 // Footer year
 const footerDate = document.getElementById("currentYear");
 
@@ -7,10 +10,19 @@ footerDate.textContent = currentYear;
 
 const NavToggleBtn = document.getElementById("navToggleButton");
 const NavBarLinks = document.getElementById("navLinksWrapper");
+const navigationToggleIcon = document.querySelector(".nav__toggle-btn--icon");
+let hidden = true;
 
 NavToggleBtn.addEventListener("click", () => {
-  const navbarStyles = NavBarLinks.classList;
-  const navToggleStyles = NavToggleBtn.classList;
-  navbarStyles.toggle("nav-links-show");
-  navToggleStyles.toggle("nav__toggle-btn-focus");
+  if (hidden) {
+    NavBarLinks.classList.add("nav-links-show");
+    NavToggleBtn.classList.add("rotate");
+    navigationToggleIcon.setAttribute("src", menuClose);
+    hidden = false;
+  } else {
+    NavBarLinks.classList.remove("nav-links-show");
+    NavToggleBtn.classList.remove("rotate");
+    navigationToggleIcon.setAttribute("src", menuOpen);
+    hidden = true;
+  }
 });
